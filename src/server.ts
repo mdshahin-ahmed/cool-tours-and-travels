c25574cf744efa3a55352d0ba2eae19f3778aa3c
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2023-11-21 23:04:19 +0600

    Connected with server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
-import express from 'express'
-const app = express()
+import app from './app'
+import mongoose from 'mongoose'
+import config from './config'
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const port = 5000
+
+// getting-started.js
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url_local)
+    console.log('connected to mongoDB')
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+server().catch((err) => console.log(err))
